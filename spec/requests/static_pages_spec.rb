9400a040015a141d7712eca82267d32f7b9ef02d
Rob Coon <rob.coon@gmail.com>
2014-07-23 23:50:11 -0700

    added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Static Pages" do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
 
   	it "should have the content 'Sample App'" do
@@ -12,7 +13,7 @@ describe "Static Pages" do
    	it "should have the title 'Ruby on Rails Tutorial Sample App | Home" do
   		visit '/static_pages/home'
   		page.should have_selector('title', 
-  			:text => "Ruby on Rails Tutorial Sample App | Home")
+  			:text => "#{base_title} | Home")
   	end
 
   end
@@ -27,7 +28,7 @@ describe "Static Pages" do
   	it "should have the title 'Ruby on Rails Tutorial Sample App | Help" do
   		visit '/static_pages/help'
   		page.should have_selector('title', 
-  			:text => "Ruby on Rails Tutorial Sample App | Help")
+  			:text => "#{base_title} | Help")
   	end
 
   end
@@ -42,8 +43,22 @@ describe "Static Pages" do
   	it "should have the title 'Ruby on Rails Tutorial Sample App | About" do
   		visit '/static_pages/about'
   		page.should have_selector('title', 
-  			:text => "Ruby on Rails Tutorial Sample App | About")
+  			:text => "#{base_title} | About")
   	end
 
+  end
+
+  describe "Contact page" do
+
+  	it "should have the content 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('h1', :text => 'Contact')
+  	end
+
+  	it "should have title 'Ruby on Rails Tutorial Sample App | Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('title',
+  			:text => "Ruby on Rails Tutorial Sample App | Contact")
+  	end
   end
 end
